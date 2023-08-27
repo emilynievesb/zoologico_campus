b7a6da86fe86318788ea54d539596b4cd0a91d33
ADEP-123 <andep210399@gmail.com>
2023-08-27 18:27:44 -0500

    feat: :sparkles: consultas capacitaciones

---
+++ src/collections/capacitaciones.js
@@ -0,0 +1,175 @@
+import autoIncrementID from "../utils/autoIncrement.js";
+import connection from "../utils/connect.js";
+
+class Capacitaciones {
+    id;
+    id_seguimiento;
+    nombre;
+    fecha;
+    descripcion;
+
+    constructor() { }
+
+    async connect() {
+        try {
+            const result = await connection("capacitaciones");
+            return result;
+        } catch (error) {
+            throw error;
+        }
+    }
+
+    async obtenerCapacitaciones() {
+        try {
+            this.session = await startTransaction();
+            const connection = await this.connect();
+            const resultado = await connection
+                .aggregate([
+                    {
+                        $project: {
+                            _id: 0,
+                            id: "$id",
+                            seguimiento: "$id_seguimiento",
+                            nombre: "$nombre",
+                            fecha: "$fecha",
+                            descripcion: "$descripcion"
+                        },
+                    },
+                ])
+                .toArray();
+            await this.session.commitTransaction();
+            return resultado;
+        } catch (error) {
+            if (this.session) {
+                await this.session.abortTransaction();
+            }
+            throw error;
+        } finally {
+            if (this.session) {
+                this.session.endSession();
+            }
+        }
+    }
+
+    async obtenerCapacitacionPorId(id) {
+        try {
+            this.session = await startTransaction();
+            const connection = await this.connect();
+            const resultado = await connection
+                .aggregate([
+                    {
+                        $match: {
+                            id: Number(id),
+                        },
+                    },
+                    {
+                        $project: {
+                            _id: 0,
+                            id: "$id",
+                            seguimiento: "$id_seguimiento",
+                            nombre: "$nombre",
+                            fecha: "$fecha",
+                            descripcion: "$descripcion"
+                        },
+                    },
+                ])
+                .toArray();
+            await this.session.commitTransaction();
+            return resultado;
+        } catch (error) {
+            if (this.session) {
+                await this.session.abortTransaction();
+            }
+            throw error;
+        } finally {
+            if (this.session) {
+                this.session.endSession();
+            }
+        }
+    }
+
+    async agregarCapacitacion() {
+        try {
+            const incremental = await autoIncrementID("capacitaciones");
+            const { id, session: newSession } = incremental;
+            this.session = newSession;
+            const connection = await this.connect();
+            const resultado = await connection.insertOne({
+                id: Number(id),
+                seguimiento: this.id_seguimiento,
+                nombre: this.nombre,
+                fecha: this.fecha,
+                descripcion: this.descripcion
+            });
+            await this.session.commitTransaction();
+            return resultado;
+        } catch (error) {
+            if (this.session) {
+                await this.session.abortTransaction();
+            }
+            throw error;
+        } finally {
+            if (this.session) {
+                this.session.endSession();
+            }
+        }
+    }
+
+    async eliminarCapacitacion(id) {
+        try {
+            this.session = await startTransaction();
+            const connection = await this.connect();
+            const resultado = await connection
+                .deleteOne(
+                    {
+                        id: Number(id)
+                    }
+                ).toArray();
+            await this.session.commitTransaction();
+            return resultado;
+        } catch (error) {
+            if (this.session) {
+                await this.session.abortTransaction();
+            }
+            throw error;
+        } finally {
+            if (this.session) {
+                this.session.endSession();
+            }
+        }
+    }
+
+    async actualizarCapacitacion() {
+        try {
+            this.session = await startTransaction();
+            const connection = await this.connect();
+            const resultado = await connection
+                .updateOne(
+                    {
+                        id: this.id
+                    },
+                    {
+                        $set: {
+                            id_seguimiento: this.id_seguimiento,
+                            nombre: this.nombre,
+                            fecha: this.fecha,
+                            descripcion: this.descripcion
+                        }
+                    }
+                ).toArray();
+            await this.session.commitTransaction();
+            return resultado;
+        } catch (error) {
+            if (this.session) {
+                await this.session.abortTransaction();
+            }
+            throw error;
+        } finally {
+            if (this.session) {
+                this.session.endSession();
+            }
+        }
+    }
+}
+
+export { Capacitaciones };
