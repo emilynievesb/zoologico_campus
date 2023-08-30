ec8a5c8035f8c3d55301c25636090382c61d332b
ADEP-123 <andep210399@gmail.com>
2023-08-29 21:42:10 -0500

    fix: :bug: arreglando iniciador de rutas put

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,12 +3,14 @@ import { postInitRoute } from "./postRoutes.js";
 import { getInitRoute } from "./getRoutes.js";
 import { deleteRoute } from "./deleteRoutes.js";
 import { middlewareRateLimit } from "../middlewares/middlewareRateLimit.js";
+import { putInitRoute } from "./putRoutes.js";
 
 const initAPIRoutes = () => {
   const router = Router();
   router.use("/get", middlewareRateLimit, getInitRoute());
   router.use("/post", middlewareRateLimit, postInitRoute());
   router.use("/delete", middlewareRateLimit, deleteRoute());
+  router.use("/put", middlewareRateLimit, putInitRoute());
   return router;
 };
 
