108804d929c8481f56ff65a7c06644c86af3d0d3
Emily <86448476+emilynievesb@users.noreply.github.com>
2023-09-02 18:40:31 -0500

    Revert "hotfixx/sprint-3/ZDB-18"

--- src/utils/autoIncrement.js
+++
@@ -1,8 +0,0 @@
-import { Counters } from "../collections/counters.js";
-
-export default async function autoIncrementID(col) {
-  const nuevoId = new Counters();
-  nuevoId.collection = col;
-  const res = await nuevoId.getID();
-  return res;
-}
