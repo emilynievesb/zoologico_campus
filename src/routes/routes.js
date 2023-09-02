1b53dfc82dd399855457a6a2bce9749bb685f6e2
emilynievesb <nemily1710@gmail.com>
2023-09-02 13:19:06 -0500

    feat: :sparkles: Versiones
    Se realiza función para versionar endpoints

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,13 +3,20 @@ import { postInitRoute } from "./postRoutes.js";
 import { getInitRoute } from "./getRoutes.js";
 import { deleteRoute } from "./deleteRoutes.js";
 import { middlewareRateLimit } from "../middlewares/middlewareRateLimit.js";
+import { putInitRoute } from "./putRoutes.js";
 
-const initAPIRoutes = () => {
+const v1Routes = () => {
   const router = Router();
   router.use("/get", middlewareRateLimit, getInitRoute());
   router.use("/post", middlewareRateLimit, postInitRoute());
+  return router;
+};
+
+const v2Routes = () => {
+  const router = Router();
+  router.use("/put", middlewareRateLimit, putInitRoute());
   router.use("/delete", middlewareRateLimit, deleteRoute());
   return router;
 };
 
-export { initAPIRoutes };
+export { v1Routes, v2Routes };
